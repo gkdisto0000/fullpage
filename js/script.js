624b01c93df55ba357c7aba43d085e2b41ebc8c3
gkdisto0000 <qufqlcto@naver.com>
2024-01-05 09:32:54 +0900

    240105_01

--- js/script.js
+++ js/script.js
@@ -1,22 +1,44 @@
 $(function() {
     // 풀페이지
-    var page = $('.fullpage').fullpage({
-        scrollingSpeed: 500,
+    $('.fullpage').fullpage({
         normalScrollElements: '.scroll-active',
-        scrollOverflow: true,
-
         //aos        
         afterLoad: function(anchorLink, index){
             jQuery('.section.active [data-aos]').addClass("aos-animate");
-            if (index == 3){
+
+            if (index == 1){
+				$('.topBtn').addClass('hide');
+                $('.gnbWrap').removeClass('black');
+            } else if (index == 2){
+				$('.topBtn').removeClass('hide');
+                $('.topBtn').removeClass('black');
+                $('.gnbWrap').removeClass('black');
+            } else if (index == 3){
 				$('.topBtn').addClass('black');
                 $('.gnbWrap').addClass('black');
 			} else if (index == 4){
 				$('.topBtn').addClass('black');
                 $('.gnbWrap').addClass('black');
+
+                //목업 슬라이드
+                var mockUpSlide = new Swiper('.mockUp-slide', {
+                    slidesPerView : 1,
+                    autoplay : true,
+                    speed : 600,
+                    loop : true,
+                });
             } else if (index == 5){
 				$('.topBtn').addClass('black');
                 $('.gnbWrap').addClass('black');
+
+                //목업 슬라이드
+                var mockUpSlide = new Swiper('.mockUp-slide2', {
+                    slidesPerView : 1,
+                    autoplay : true,
+                    speed : 600,
+                    loop : true,
+                });
+            
             } else if (index == 6){
 				$('.topBtn').addClass('black');
                 $('.gnbWrap').addClass('black');
@@ -26,14 +48,6 @@ $(function() {
             } else if (index == 8){
 				$('.topBtn').addClass('black');
                 $('.gnbWrap').addClass('black');
-                autoScrolling : false
-
-            } else if (index == 1){
-				$('.topBtn').addClass('hide');
-                $('.gnbWrap').removeClass('black');
-            } else if (index == 2){
-				$('.topBtn').removeClass('hide');
-                $('.gnbWrap').removeClass('black');
             } else {
                 $('.topBtn').removeClass('black');
                 $('.topBtn').removeClass('hide');
@@ -59,20 +73,6 @@ $(function() {
 	});
 
 
-    // sec04, 05 목업 슬라이드
-    var mockUpSlide = new Swiper('.mockUp-slide', {
-        slidesPerView : 1,
-        autoplay : false,
-        loop : true,
-    });
-
-    var mockUpSlide = new Swiper('.mockUp-slide2', {
-        slidesPerView : 1,
-        autoplay : false,
-        loop : true,
-    });
-
-
     // 작가 슬라이드
 	var swiperArticle = new Swiper('.art-slideWrap', {
 		spaceBetween : 40,
@@ -86,7 +86,6 @@ $(function() {
             type: 'bullets',
             clickable: true, 
         },
-
 		 breakpoints: {
             1350: {
                 spaceBetween : 20,
@@ -114,26 +113,57 @@ $(function() {
 		},
 	});
 
-    
-    // sec08 tab 작동
-    $('.yearTab li').on('click',function(){
-		var tabNum = $(this).data('yearTab');
-		$('.yearTab li').removeClass('on');
-		$(this).addClass('on');
-		$('#' + tabNum).css('height','auto');
-	});
 
+    // sec08 tab 작동
 	$(".yearTab li").click(function(){
 		var tabNum = $(this).data('year');
 		$(".yearTab li").removeClass('on');
 		$(this).addClass('on');
 		$('.listWrap').removeClass('on');
 		$('#' + tabNum).addClass('on');
+
+        var list = $('.listWrap.on .scroll-all');
+        var listNum = list.children().length;
+    
+        // 더보기 버튼 -> list 4개 이상일때 노출
+        if(listNum > 4) {
+            $('.listMore').removeClass('hide');
+            list.parent('.listWrap').addClass('scroll-active');
+        } else {
+            $('.listMore').addClass('hide');
+            list.parent('.listWrap').removeClass('scroll-active');
+        }
+
+        $(".listWrap.on .scroll-all").scroll(function(){    
+            var scrollTop = $(this).scrollTop();
+            var innerHeight = $(this).innerHeight();
+            var scrollHeight = $(this).prop('scrollHeight');
+            if (scrollTop + innerHeight >= scrollHeight) {
+                location.href = "#sec09"
+            } else if (scrollTop == 0) {
+                location.href = "#sec07"
+            }
+            console.log(scrollTop);
+        });
+
+
+
 	});
 	$('.yearTab li:nth-child(1)').trigger('click');
+    
 
+
+    $('.listMore').click(function(){
+        //더보기 클릭하면 스크롤 맨 하단으로 이동!!
+    });
+
+    
+    //mo 햄버거 바
     $('.mapBtn').click(function(){
         $(this).toggleClass('active');
     });
 
-})
+
+
+}) 
+// end
